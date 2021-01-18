222a74f18081395f508f1f7a4625e1770c46def8
admin <limchw21@gmail.com>
2021-01-18 22:04:09 +0900

    Change the title error

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,16 +11,17 @@ class Post(models.Model):
     created_date=models.DateTimeField(
         default = timezone.now)
     published_date=models.DateTimeField(
-        blank=True, null=True
+        default = timezone.now
+        #blank=True, null=True
     )
 
 
-def publish(self):
-    self.published_date=timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
 # Create your models here.
